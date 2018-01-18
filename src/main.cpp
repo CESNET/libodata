9602ac41e9e961c415f73b7998523099d032acd2
Jakub Valenta <33723739+jakub-valenta@users.noreply.github.com>
2018-01-18 10:19:43 +0100

    Project structure

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <restclient-cpp/restclient.h>
+
+int main(int argc, char** argv) {
+    RestClient::Response res = RestClient::get("https://dhr1.cesnet.cz/odata/v1/Products('32d27303-8a3c-4088-8a09-4e4816174d6b')/Nodes");
+    std::cout << "Response code: " << res.code << std::endl;
+    return 0;
+}
