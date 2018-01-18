e7de9abb84924a5eae500d5a1f8569c930ab94d2
Jakub Valenta <33723739+jakub-valenta@users.noreply.github.com>
2018-01-18 15:16:28 +0100

    Authentication demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include <restclient-cpp/restclient.h>
+#include <restclient-cpp/connection.h>
 
-int main(int, char**) {
-    RestClient::Response res = RestClient::get("https://dhr1.cesnet.cz/odata/v1/Products('32d27303-8a3c-4088-8a09-4e4816174d6b')/Nodes");
+int main(int argc, char** argv) {
+    if(argc < 3) {
+        return 1;
+    }
+    RestClient::Connection connection("https://dhr1.cesnet.cz/odata/v1/");
+    connection.SetBasicAuth(argv[1], argv[2]);
+    auto res = connection.get("Products('32d27303-8a3c-4088-8a09-4e4816174d6b')/$value");
     std::cout << "Response code: " << res.code << std::endl;
+    std::cout << res.body << std::endl;
     return 0;
 }
